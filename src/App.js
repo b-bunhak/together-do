6e9cebdde16c6974574777d4b5757dd9dce62cd5
Bruno Bunhak <b.bunhak@gmail.com>
2019-03-06 19:39:19 -0300

    Items em Firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 import { Route, Switch, Redirect } from 'react-router-dom';
 
@@ -38,49 +39,53 @@ const App = () => {
 		}
 	}, [usuario]);
 
-	const [items, setItems] = useState(
-		new Map([
-			['1', { id: '1', item: 'Item', feito: false }],
-			['2', { id: '2', item: 'Item 2', feito: false }],
-			['3', { id: '3', item: 'Item 2', feito: false }],
-			['4', { id: '4', item: 'Item 2', feito: false }],
-			['5', { id: '5', item: 'Item 2', feito: false }],
-			['6', { id: '6', item: 'Item 2', feito: false }],
-			['7', { id: '7', item: 'Item 2', feito: false }],
-			['8', { id: '8', item: 'Item 2', feito: false }],
-			['9', { id: '9', item: 'Item 2', feito: false }],
-			['10', { id: '10', item: 'Item 2', feito: false }],
-			['11', { id: '11', item: 'Item 2', feito: false }],
-			['12', { id: '12', item: 'Item 2', feito: false }],
-			['13', { id: '13', item: 'Item 2', feito: false }],
-			['14', { id: '14', item: 'Item 2', feito: false }]
-		])
-	);
+	const [items, setItems] = useState(new Map());
+
+	useEffect(() => {
+		if (usuario) {
+			return firebase
+				.firestore()
+				.collection('items')
+				.onSnapshot(snapshot => {
+					const items = new Map();
+
+					snapshot.forEach(doc => {
+						items.set(doc.id, doc.data());
+					});
+
+					setItems(items);
+				});
+		}
+	}, [usuario]);
 
 	function adicionarItem(item) {
-		const novoItems = new Map(items);
+		const itemDoc = firebase
+			.firestore()
+			.collection('items')
+			.doc();
 
-		const itemId = (novoItems.size + 1).toString();
-
-		novoItems.set(itemId, { id: itemId, ...item });
-		setItems(novoItems);
+		return itemDoc.set({ ...item, id: itemDoc.id });
 	}
 
 	function editarItem(item) {
-		const novoItems = new Map(items);
+		if (item.id && items.has(item.id)) {
+			const itemDoc = firebase
+				.firestore()
+				.collection('items')
+				.doc(item.id);
 
-		if (item.id && novoItems.has(item.id)) {
-			novoItems.set(item.id, { ...item });
-			setItems(novoItems);
+			return itemDoc.set({ ...item, id: itemDoc.id }, { merge: true });
 		}
 	}
 
 	function deletarItem(id) {
-		const novoItems = new Map(items);
+		if (id && items.has(id)) {
+			const itemDoc = firebase
+				.firestore()
+				.collection('items')
+				.doc(id);
 
-		if (id && novoItems.has(id)) {
-			novoItems.delete(id);
-			setItems(novoItems);
+			return itemDoc.delete();
 		}
 	}
 
