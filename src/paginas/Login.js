5d9f730bb1723c4881e67f193371022cc518f1f8
Bruno Bunhak <b.bunhak@gmail.com>
2019-06-25 16:54:27 -0300

    Login facebook

--- src/paginas/Login.js
+++ src/paginas/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import * as firebase from 'firebase/app';
 import { withStyles } from '@material-ui/core/styles';
+import { Typography, Box } from '@material-ui/core';
 
 const styles = theme => ({
 	'@global': {
@@ -15,6 +16,7 @@ const styles = theme => ({
 		height: '100%',
 		padding: theme.spacing(2),
 		display: 'flex',
+		flexDirection: 'column',
 		justifyContent: 'center',
 		alignItems: 'center'
 	},
@@ -30,16 +32,27 @@ const uiConfig = {
 
 	signInSuccessUrl: '/',
 
-	signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID]
+	signInOptions: [
+		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+		firebase.auth.FacebookAuthProvider.PROVIDER_ID
+	]
 };
 
 const Login = ({ classes }) => (
 	<div className={classes.pagina}>
-		<StyledFirebaseAuth
-			uiConfig={uiConfig}
-			firebaseAuth={firebase.auth()}
-			className={classes.authContainer}
-		/>
+		<Box clone my="auto">
+			<Typography variant="h3" component="div">
+				Together-Do
+			</Typography>
+		</Box>
+
+		<Box clone mb="auto">
+			<StyledFirebaseAuth
+				uiConfig={uiConfig}
+				firebaseAuth={firebase.auth()}
+				className={classes.authContainer}
+			/>
+		</Box>
 	</div>
 );
 
